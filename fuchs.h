e63eabfc8be7e215fc792d254ca53239ce9dfbab
Nikita <mistertwotch@gmail.com>
2021-10-25 19:05:41 +0300

    Added funcs.h

---
+++ fuchs.h
@@ -0,0 +1,45 @@
+#include <iostream>
+#include "windows.h"
+#include <cmath>
+using namespace std;
+void deystvie(double a,double b) {
+	char dey;
+	cin >> dey;
+	double otvet;
+	switch (dey) {
+	case '+':
+		otvet = a + b;
+		cout << "Ответ: " << otvet << endl;
+		break;
+	case '-':
+		otvet = a - b;
+		cout << "Ответ: " << otvet << endl;
+		break;
+		//Half Ready
+	case '*':
+		otvet = a * b;
+		cout << "Ответ: " << otvet << endl;
+		break; // Ready
+	case '/':
+		otvet = a * b;
+		cout << "Ответ: " << otvet << endl;
+		break;//ready
+	default:
+		cout << "Неверный ввод";
+		otvet = 10;
+		break;
+	}
+	if (otvet >= 0)
+		cout << "Осталось " << otvet << " секунд до взрыва";
+	if (otvet < 0) {
+		otvet *= -1;
+		cout << "Осталось " << otvet << "Секунд до взрыва";
+	}
+	for (int i = round(otvet);i > 0;i--) {
+		cout << endl << i;
+		Beep(5000, 200);
+		Sleep(1000);
+	}
+	Beep(10000, 2000);
+	Sleep(1000);
+}
--- pridey.cpp
+++ pridey.cpp
@@ -1,7 +1,5 @@
-#include <iostream>
-#include <cmath>
+#include "fuchs.h"
 #include <string>
-#include "windows.h"
 using namespace std;
 int main() {
 	setlocale(LC_ALL, "Russian");
@@ -16,46 +14,8 @@ int main() {
 	cin >> a;
 	cout << "Второе число: ";
 	cin >> b;
-	cout  <<"Выберте нужное математическое действие: - + / * " << endl;
-	char dey;
-	cin >> dey;
-	double otvet;
-	switch (dey) {
-	case '+':
-		otvet = a + b;
-		cout << "Ответ: " <<otvet<<endl;
-		break;
-	case '-':
-		otvet = a - b;
-		cout << "Ответ: " << otvet << endl;
-		break; 
-		//Half Ready
-	case '*':
-		otvet = a * b;
-		cout << "Ответ: " << otvet << endl;
-		break; // Ready
-	case '/':
-		otvet = a * b;
-		cout << "Ответ: " << otvet << endl;
-		break;//ready
-	default:
-		cout << "Неверный ввод";
-		otvet = 10;
-		break;
-	}
-	if (otvet>=0)
-	cout << "Осталось "<<otvet  << " секунд до взрыва";
-	if (otvet < 0) {
-		otvet *= -1;
-		cout << "Осталось " << otvet << "Секунд до взрыва";
-	}
-	for (int i = round(otvet);i > 0;i--) {
-		cout <<endl<< i;
-		Beep(5000, 200);
-		Sleep(1000);
-	}
-	Beep(10000, 2000);
-	Sleep(1000);
+	cout << "Выберте нужное математическое действие: - + / * " << endl;
+	deystvie(a,b);
 	cout << endl;
 	return 0;
 }
